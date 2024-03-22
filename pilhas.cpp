4f8b063721489e680354db521e673300704543ed
Igor <156442529+IgorPedrosoLolli@users.noreply.github.com>
2024-03-22 14:24:02 -0300

    Add files via upload

---
+++ pilhas.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+	
+	int num, aux_num;
+	int *vet;
+	int i = 0, j = 0;
+	
+	printf("Digite um numero: ");
+	scanf("%d", &num);
+	
+	vet = (int *) malloc(num*sizeof(int) );
+	
+	do{
+		aux_num = num % 2;
+		vet[i] = aux_num;
+		i++;
+		num = num / 2;
+				
+	}while(num > 0);
+	
+	printf("\n");
+	printf("Valor na pilha com a leitura convertida!\n");
+	
+	for(j = i - 1; j >= 0; j--)
+		printf("%d", vet[j]);
+	
+	free (vet);
+		
+	return 0;
+}
